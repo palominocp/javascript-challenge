49c13a7a80ad782a5df7b38b419e27d2ea0c91cf
palominocp <palominocp@outlook.com>
2020-09-28 23:04:01 -0500

    UFO Level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,17 +4,37 @@ var tableData = data;
 // YOUR CODE HERE!
 
 const element = document.querySelector("tbody");
-element.innerHTML = "<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td></tr>";
 
 var tableContent = "";
-
 for (index = 0; index < data.length; index++) {
-    tableContent += "<tr><td>" + data[index]['datetime'] + "</td>" + 
+    tableContent += "<tr>" + 
+    "<td>" + data[index]['datetime'] + "</td>" + 
     "<td>" + data[index]['city'] + "</td>" + 
     "<td>" + data[index]['state'] + "</td>" + 
     "<td>" + data[index]['country'] + "</td>" + 
     "<td>" + data[index]['shape'] + "</td>" +
     "<td>" + data[index]['duration'] + "</td>" + 
-    "<td>" + data[index]['comments'] + "</td>" + "</tr>";
+    "<td>" + data[index]['comments'] + "</td>" + 
+    "</tr>";
 }
 element.innerHTML = tableContent;
+
+d3.select("#filter-btn").on("click", function() {
+  var dateInput = d3.select("#datetime").property("value");
+
+  var tableContent = "";
+  for (index = 0; index < data.length; index++) {
+    if (new Date(data[index]['datetime']).getTime() == new Date(dateInput).getTime()) {
+      tableContent += "<tr>" + 
+      "<td>" + data[index]['datetime'] + "</td>" + 
+      "<td>" + data[index]['city'] + "</td>" + 
+      "<td>" + data[index]['state'] + "</td>" + 
+      "<td>" + data[index]['country'] + "</td>" + 
+      "<td>" + data[index]['shape'] + "</td>" +
+      "<td>" + data[index]['duration'] + "</td>" + 
+      "<td>" + data[index]['comments'] + "</td>" + 
+      "</tr>";
+    }
+  }
+  element.innerHTML = tableContent;
+});

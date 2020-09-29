f9a54a84637212f55b10af64ec14e79525dff55c
palominocp <palominocp@outlook.com>
2020-09-28 23:56:21 -0500

    UFO Level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,51 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+const element = document.querySelector("tbody");
+
+var tableContent = "";
+for (index = 0; index < data.length; index++) {
+    tableContent += "<tr>" + 
+    "<td>" + data[index]['datetime'] + "</td>" + 
+    "<td>" + data[index]['city'] + "</td>" + 
+    "<td>" + data[index]['state'] + "</td>" + 
+    "<td>" + data[index]['country'] + "</td>" + 
+    "<td>" + data[index]['shape'] + "</td>" +
+    "<td>" + data[index]['duration'] + "</td>" + 
+    "<td>" + data[index]['comments'] + "</td>" + 
+    "</tr>";
+}
+element.innerHTML = tableContent;
+
+d3.select("#filter-btn").on("click", function() {
+  var dateInput = d3.select("#datetime").property("value");
+  var cityInput = d3.select("#city").property("value");
+  var stateInput = d3.select("#state").property("value");
+  var countryInput = d3.select("#country").property("value");
+  var shapeInput = d3.select("#shape").property("value");
+
+  var tableContent = "";
+  for (index = 0; index < data.length; index++) {
+    if (dateInput === "" || new Date(dateInput).getTime() === new Date(data[index]['datetime']).getTime()) {
+      if (cityInput === "" || cityInput === data[index]['city']) {
+        if (stateInput === "" || stateInput === data[index]['state']) {
+          if (countryInput === "" || countryInput === data[index]['country']) {
+            if (shapeInput === "" || shapeInput === data[index]['shape']) {
+              tableContent += "<tr>" + 
+              "<td>" + data[index]['datetime'] + "</td>" + 
+              "<td>" + data[index]['city'] + "</td>" + 
+              "<td>" + data[index]['state'] + "</td>" + 
+              "<td>" + data[index]['country'] + "</td>" + 
+              "<td>" + data[index]['shape'] + "</td>" +
+              "<td>" + data[index]['duration'] + "</td>" + 
+              "<td>" + data[index]['comments'] + "</td>" + 
+              "</tr>";
+            }
+          }
+        }
+      }
+    }
+  }
+  element.innerHTML = tableContent;
+});
